ee301f024f7ceaa5222a509d48ea3943db065c40
Hannah S <yihanshannn@gmail.com>
2023-02-10 14:03:52 -0500

    Add requirements.txt

--- test.py
+++ test.py
@@ -5,32 +5,45 @@ import pandas as pd
 
 
 
-def Basic_Auth(username, password):
+
+
+
+# Insert the Anaplan account email being used
+# username = 'hshan@lionpointgroup.com'
+# password = 'Dacai@2315syh'
+#
+
+def get_actions_ids(username,password, wGuid, mGuid):
     user = 'Basic ' + str(base64.b64encode((f'{username}:{password}'
                                             ).encode('utf-8')).decode('utf-8'))
     headers = {'Authorization': user}
-    return headerss
-
-headerss = Basic_Auth(username, password)
-def get_workspace_ids(wGuid, mGuid):
-    # user = 'Basic ' + str(base64.b64encode((f'{username}:{password}'
-    #                                         ).encode('utf-8')).decode('utf-8'))
-    # headers = {'Authorization': user}
     url = f'https://api.anaplan.com/1/3/workspaces/{wGuid}/' + f'models/{mGuid}/actions'
-    response = requests.get(url, headers=headerss)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         df = pd.json_normalize(data)
-        print(df)
+        df.astype(str)
+        st.dataframe(df)
+        st.download_button("Download as CSV", df.to_csv(),file_name='Actions.csv', mime = 'text/csv')
     else:
         return None
 
+
+
+def main():
+    st.title('Get Action IDs :blue_book:')
+    username = st.text_input("Enter username: ")
+    password = st.text_input("Enter password: ",type="password")
+    wGuid = st.text_input("Enter Workspace ID: ")
+    mGuid = st.text_input("Enter Model ID: ")
+    if st.button("Send"):
+        if username and password:
+           Actions = get_actions_ids(username, password,wGuid,mGuid)
+        else:
+           st.write("Failed to retrieve Actions.")
+
+
+
 if __name__ == '__main__':
-    # st.title('Get Workspace IDs ^_^')
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    Workspaces = get_workspace_ids(username, password)
-
-
-
+    main()
